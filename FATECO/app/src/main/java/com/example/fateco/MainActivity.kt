b72ad918c73670a9c1dda4ba4ffff7c36e03146c
RenanHMF <renanhmf2@gmail.com>
2022-05-16 23:00:51 -0300

    feat: requestFocus() dos EditText e setKeyListener(null) para evitar alteração

--- FATECO/app/src/main/java/com/example/fateco/MainActivity.kt
+++ FATECO/app/src/main/java/com/example/fateco/MainActivity.kt
@@ -1,13 +1,14 @@
 package com.example.fateco
 
 import android.graphics.Color
-import android.graphics.PorterDuff
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,14 +19,15 @@ class MainActivity : AppCompatActivity() {
         val txtFATECO : TextView = findViewById(R.id.txtFATECO)
         val btnVerificarResposta : Button = findViewById(R.id.btnVerificarResposta)
 
+        val letra0 : EditText = findViewById(R.id.letra0)
+        val letra1 : EditText = findViewById(R.id.letra1)
+        val letra2 : EditText = findViewById(R.id.letra2)
+        val letra3 : EditText = findViewById(R.id.letra3)
+        val letra4 : EditText = findViewById(R.id.letra4)
+
         btnVerificarResposta.setOnClickListener {
             val palavraSecreta = "FATEC"
 
-            val letra0 : EditText = findViewById(R.id.letra0)
-            val letra1 : EditText = findViewById(R.id.letra1)
-            val letra2 : EditText = findViewById(R.id.letra2)
-            val letra3 : EditText = findViewById(R.id.letra3)
-            val letra4 : EditText = findViewById(R.id.letra4)
 
             val listaPalavraTentada = mutableListOf<EditText>()
 
@@ -51,7 +53,93 @@ class MainActivity : AppCompatActivity() {
                     listaPalavraTentada[i].setBackgroundColor(Color.RED)
                     listaPalavraTentada[i].setTextColor(Color.BLACK)
                 }
+
+                letra0.setKeyListener(null)
+                letra1.setKeyListener(null)
+                letra2.setKeyListener(null)
+                letra3.setKeyListener(null)
+                letra4.setKeyListener(null)
             }
         }
+
+        letra0.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                if (s.length == 1) {
+                    letra1.requestFocus()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+            }
+        })
+
+        letra1.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                if (s.length == 1) {
+                    letra2.requestFocus()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+            }
+        })
+        letra2.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                if (s.length == 1) {
+                    letra3.requestFocus()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+            }
+        })
+
+        letra3.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                if (s.length == 1) {
+                    letra4.requestFocus()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+            }
+        })
+
     }
 }
